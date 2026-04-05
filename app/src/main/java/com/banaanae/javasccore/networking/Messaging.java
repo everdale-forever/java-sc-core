62a1ea052e8f3befe25247a16bdaf985449db531
Banaanae <83927639+Banaanae@users.noreply.github.com>
2026-04-05 15:32:37 +1000

    messaging: sendToSessions and sendToAll

--- app/src/main/java/com/banaanae/javasccore/networking/Messaging.java
+++ app/src/main/java/com/banaanae/javasccore/networking/Messaging.java
@@ -1,11 +1,16 @@
 package com.banaanae.javasccore.networking;
 
+import com.banaanae.javasccore.Server;
 import com.banaanae.javasccore.titan.Debugger;
 import com.banaanae.javasccore.Server.Client;
 import com.banaanae.javasccore.titan.ArrayUtils;
 import com.banaanae.javasccore.titan.datastream.bytestream.ByteStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public abstract class Messaging {
     protected Client session;
@@ -43,6 +48,106 @@ public abstract class Messaging {
         } catch (IOException ex) {
             System.getLogger(Messaging.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
+    }
+    
+    public void sendToSession(int sessionId, boolean doNotEncrypt) {
+        try {
+            byte[] payload = getPayload(doNotEncrypt);
+            
+            final Client session = Server.sessions.get(sessionId);
+            
+            OutputStream out = session.getOutputStream();
+            out.write(payload, 0, payload.length);
+            out.flush();
+        } catch (IOException ex) {
+            System.getLogger(Messaging.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+    }
+    
+    public void sendToSession(int sessionId) {
+        try {
+            byte[] payload = getPayload(false);
+            
+            final Client session = Server.sessions.get(sessionId);
+            
+            OutputStream out = session.getOutputStream();
+            out.write(payload, 0, payload.length);
+            out.flush();
+        } catch (IOException ex) {
+            System.getLogger(Messaging.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+    }
+    
+    public void sendToSessions(int[] sessionIds, boolean doNotEncrypt) {
+        List selectedSessions = Arrays.stream(sessionIds)
+            .mapToObj(Server.sessions::get)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+        
+        byte[] payload = getPayload(false);
+        
+        if (!selectedSessions.isEmpty()) {
+            for (int sessionId : Server.sessions.keySet()) {
+                try {
+                    OutputStream out = Server.sessions.get(sessionId).getOutputStream();
+                    out.write(payload, 0, payload.length);
+                    out.flush();
+                } catch (IOException ex) {
+                    System.getLogger(Messaging.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+                }
+            }
+        }
+    }
+    
+    public void sendToSessions(int[] sessionIds) {
+        List selectedSessions = Arrays.stream(sessionIds)
+            .mapToObj(Server.sessions::get)
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+        
+        byte[] payload = getPayload(false);
+        
+        if (!selectedSessions.isEmpty()) {
+            for (int sessionId : Server.sessions.keySet()) {
+                try {
+                    OutputStream out = Server.sessions.get(sessionId).getOutputStream();
+                    out.write(payload, 0, payload.length);
+                    out.flush();
+                } catch (IOException ex) {
+                    System.getLogger(Messaging.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+                }
+            }
+        }
+    }
+    
+    public void sendToAll(boolean doNotEncrypt) {
+        byte[] payload = getPayload(false); // TODO: Crypto
+        
+        for (int sessionId : Server.sessions.keySet()) {
+            try {
+                OutputStream out = Server.sessions.get(sessionId).getOutputStream();
+                out.write(payload, 0, payload.length);
+                out.flush();
+            } catch (IOException ex) {
+                System.getLogger(Messaging.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+            }
+        }
+            
+    }
+    
+    public void sendToAll() {
+        byte[] payload = getPayload(false); // TODO: Crypto
+        
+        for (int sessionId : Server.sessions.keySet()) {
+            try {
+                OutputStream out = Server.sessions.get(sessionId).getOutputStream();
+                out.write(payload, 0, payload.length);
+                out.flush();
+            } catch (IOException ex) {
+                System.getLogger(Messaging.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+            }
+        }
+            
     }
     
     private byte[] getPayload(boolean doNotEncrypt) {
